b4f91e3516a4b39e5f27c287e7b82a81cc80075a
Aaron MacPherson <aaronmacpherson@outlook.com>
2025-04-08 16:58:05 -0300

    coded filtering for manage view

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,9 +4,20 @@ function confirmDelete(event) {
     }
 }
 
+function setFilter(content, filter) {
+    filter.classList.toggle("filter-button-toggle");
+    content.classList.toggle("hidden");
+}
+
 function main() {
     if (window.location.pathname == "/games" && document.getElementById("delete-link")) {
         document.getElementById("delete-link").addEventListener("click", (event) => confirmDelete(event));
+    }
+
+    if (window.location.pathname == "/manage") {
+        document.getElementById("platform-filter").addEventListener("click", () => setFilter(document.getElementById("platforms"), document.getElementById("platform-filter")));
+        document.getElementById("priority-filter").addEventListener("click", () => setFilter(document.getElementById("priorities"), document.getElementById("priority-filter")));
+        document.getElementById("statuses-filter").addEventListener("click", () => setFilter(document.getElementById("statuses"), document.getElementById("statuses-filter")));
     }
 }
 
