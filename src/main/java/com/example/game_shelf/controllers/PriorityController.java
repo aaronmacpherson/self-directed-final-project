99f2f74a112e9ed67f3603cfbeca84ac6746c9ec
Aaron MacPherson <aaronmacpherson@outlook.com>
2025-04-08 15:53:42 -0300

    coded priority controller and views

---
+++ src/main/java/com/example/game_shelf/controllers/PriorityController.java
@@ -0,0 +1,67 @@
+package com.example.game_shelf.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.game_shelf.models.Priority;
+import com.example.game_shelf.repositories.PriorityRepository;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class PriorityController {
+    @Autowired
+    private PriorityRepository priorityRepository;
+
+    @GetMapping("/priorities/add")
+    public String addPriorities(Model model) {
+        if (!model.containsAttribute("priority")) {
+            model.addAttribute("priority", new Priority());
+        }
+
+        return "addPriority";
+    }
+
+    @PostMapping("/priorities/add")
+    public String addPriorities(@Valid Priority priority, BindingResult result, Model model,
+            RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.priority", result);
+            redirectAttributes.addFlashAttribute("priority", priority);
+            return "redirect:/priorities/add";
+        }
+
+        priorityRepository.save(priority);
+        return "redirect:/manage";
+    }
+
+    @GetMapping("/priorities/edit/{id}")
+    public String editPriorities(@PathVariable long id, Model model) {
+        if (!model.containsAttribute("priority")) {
+            Priority priority = priorityRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid priority id: " + id));
+            model.addAttribute("priority", priority);
+        }
+
+        return "editPriority";
+    }
+
+    @PostMapping("/priorities/edit/{id}")
+    public String editPriorities(@PathVariable long id, @Valid Priority priority, BindingResult result, Model model,
+            RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.priority", result);
+            redirectAttributes.addFlashAttribute("priority", priority);
+            return "redirect:/priorities/edit/{id}";
+        }
+
+        priorityRepository.save(priority);
+        return "redirect:/manage";
+    }
+}
--- src/main/java/com/example/game_shelf/models/Priority.java
+++ src/main/java/com/example/game_shelf/models/Priority.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Priority {
@@ -11,21 +13,23 @@ public class Priority {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 25, message = "Name must not exceed 15 characters")
     private String name;
 
     public long getId() {
         return id;
-      }
-    
-      public void setId(long id) {
+    }
+
+    public void setId(long id) {
         this.id = id;
-      }
+    }
 
-      public String getName() {
+    public String getName() {
         return name;
-      }
+    }
 
-      public void setName(String name) {
+    public void setName(String name) {
         this.name = name;
-      }
+    }
 }
