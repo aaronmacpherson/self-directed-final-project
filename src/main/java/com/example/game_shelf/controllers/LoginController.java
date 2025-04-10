4570726317d83422e2e7687ed7795fe5555c7ef8
Aaron MacPherson <aaronmacpherson@outlook.com>
2025-04-10 16:18:54 -0300

    added redirect to login controller

--- src/main/java/com/example/game_shelf/controllers/LoginController.java
+++ src/main/java/com/example/game_shelf/controllers/LoginController.java
@@ -1,5 +1,8 @@
 package com.example.game_shelf.controllers;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("/login")
     public String login() {
+        Authentication authToken = SecurityContextHolder.getContext().getAuthentication();
+
+        // redirect to the games view if the user attempting to access the login page is already authenticated
+        if (!(authToken instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/games";
+        }
+
         return "login";
     }
 }
